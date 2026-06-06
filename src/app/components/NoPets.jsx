8238026e810427eaf0ff9530e41d1bb5bbace518
ShifathPro057 <sharifulamin2023@gmail.com>
2026-06-06 23:08:59 +0600

    NoPets add

---
+++ src/app/components/NoPets.jsx
@@ -0,0 +1,12 @@
+
+const NoPets = () => {
+    return (
+        <div>
+            <div className="flex justify-center items-center border rounded-2xl py-40 my-10">
+                <h2 className="text-4xl">No Pets Available</h2>
+            </div>
+        </div>
+    );
+};
+
+export default NoPets;
